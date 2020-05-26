a2076d836337f0929bc1e293ee80ad37b707c603
Unknown <antoine.bernier59@gmail.com>
2020-05-26 11:10:12 +0200

    [WIP]: generate controllers

---
+++ app/controllers/productions_controller.rb
@@ -0,0 +1,13 @@
+class ProductionsController < ApplicationController
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :productions, only: [:index, :new, :create, :show]
+  root 'productions#index'
 end
